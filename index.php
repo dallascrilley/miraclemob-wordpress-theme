d3b7b4f8b712625543125877e4e2e52bf6adb485
Dallas C <dallas@dallascrilley.com>
2017-11-16 11:15:19 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- Load the header -->
+<?php get_header(); ?>
+<!-- Done loading header -->
+
+
+<!-- Body content starts here -->
+<div class="main">
+  Body content
+</div>
+<!-- Body content ends here -->
+
+
+<!-- Load the footer  -->
+<?php get_footer(); ?>
+<!-- Done loading footer  -->
