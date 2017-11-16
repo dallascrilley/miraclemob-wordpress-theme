4a15de0a85dab4014b6c739a57b8e1a8e04934c8
Dallas C <dallas@dallascrilley.com>
2017-11-16 17:56:21 -0600

    various events pages improvements, including past-events page

--- front-page.php
+++ front-page.php
@@ -7,21 +7,34 @@
 <div class="page-banner">
 <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>);"></div>
   <div class="page-banner__content container t-center c-white">
-    <h1 class="headline headline--large">Welcome!</h1>
-    <h2 class="headline headline--medium">We think you&rsquo;ll like our organization.</h2>
-    <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>miracles</strong> we have posted?</h3>
-    <a href="#" class="btn btn--large btn--blue">View Our Miracles</a>
+    <h1 class="headline headline--large">Welcome to Miracle Mob!</h1>
+    <h2 class="headline headline--medium">Become a part of someone's miracle.</h2>
+    <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>candidates</strong> we have posted?</h3>
+    <a href="#" class="btn btn--large btn--blue">View Miracle Candidates</a>
   </div>
 </div>
 <div class="full-width-split group">
   <div class="full-width-split__one">
     <div class="full-width-split__inner">
-      <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
+      <h2 class="headline headline--small-plus t-center">Upcoming Miracles</h2>
 
       <?php
+      $today = date('Ymd');
+
         $homepageEvents = new WP_Query(array(
-          'posts_per_page' => 2,
-          'post_type' => 'event'
+          'posts_per_page' => -1,
+          'post_type' => 'event',
+          'meta_key' => 'event_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric'
+          )
+          )
         ));
 
         while($homepageEvents->have_posts()) {
@@ -36,19 +49,23 @@
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-              <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+              <p><?php if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 18);
+                  } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
             </div>
           </div>
         <?php }
       ?>
 
-      <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+      <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
 
     </div>
   </div>
   <div class="full-width-split__two">
     <div class="full-width-split__inner">
-      <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+      <h2 class="headline headline--small-plus t-center">From Our Blog</h2>
       <?php
         $homepagePosts = new WP_Query(array(
           'posts_per_page' => 2
@@ -70,7 +87,7 @@
       ?>
     </div>
 
-      <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+      <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
     </div>
 </div>
 
