1c8026f7d8aba68e0421f728843d6a3b0f63c60f
Dallas C <dallas@dallascrilley.com>
2017-11-16 15:57:28 -0600

    other blog post front page improvements

--- front-page.php
+++ front-page.php
@@ -46,32 +46,30 @@
   </div>
   <div class="full-width-split__two">
     <div class="full-width-split__inner">
-      <h2 class="headline headline--small-plus t-center">From Our Blog</h2>
+      <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
+      <?php
+        $homepagePosts = new WP_Query(array(
+          'posts_per_page' => 2
+        ));
 
-      <div class="event-summary">
-        <a class="event-summary__date event-summary__date--beige t-center" href="#">
-          <span class="event-summary__month">Nov</span>
-          <span class="event-summary__day">16</span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Most Inspiring</a></h5>
-          <p>We were voted #1 most inspiring this year!. <a href="#" class="nu gray">Read more</a></p>
-        </div>
-      </div>
-      <div class="event-summary">
-        <a class="event-summary__date event-summary__date--beige t-center" href="#">
-          <span class="event-summary__month">Nov</span>
-          <span class="event-summary__day">15</span>
-        </a>
-        <div class="event-summary__content">
-          <h5 class="event-summary__title headline headline--tiny"><a href="#">Our Miracles in the news</a></h5>
-          <p>Two of our miracles have been in national news lately. <a href="#" class="nu gray">Read more</a></p>
-        </div>
-      </div>
+        while ($homepagePosts->have_posts()) {
+          $homepagePosts->the_post(); ?>
+          <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+              <span class="event-summary__month"><?php the_time('M'); ?></span>
+              <span class="event-summary__day"><?php the_time('d'); ?></span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+              <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+            </div>
+          </div>
+        <?php } wp_reset_postdata();
+      ?>
+    </div>
 
       <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
     </div>
-  </div>
 </div>
 
 <div class="hero-slider">
