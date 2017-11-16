cf38a31a0e6365c4eb3cc7e6e5384af21213e0b3
Dallas C <dallas@dallascrilley.com>
2017-11-16 12:27:49 -0600

    Added base header, footer, and functions

--- footer.php
+++ footer.php
@@ -1,15 +1,27 @@
+<!-- Footer is four columns (1, 2, 3, 4 from left to right) -->
+
+<!-- Call the footer function -->
+
 <?php wp_footer(); ?>
+
+
+<!-- Start footer div -->
 <footer class="site-footer">
 
   <div class="site-footer__inner container container--narrow">
 
     <div class="group">
 
+
+      <!-- Start footer column 1 (logo and phone number) -->
       <div class="site-footer__col-one">
         <h1 class="logo-text logo-text--alt-color"><a href="#"><strong>Miracle</strong> Mob</a></h1>
         <p><a class="site-footer__link" href="#">(555) 555-5555</a></p>
       </div>
+      <!-- End footer column 1 (logo and phone number) -->
 
+
+      <!-- Start footer column 2 (main nav links) -->
       <div class="site-footer__col-two-three-group">
         <div class="site-footer__col-two">
           <h3 class="headline headline--small">Explore</h3>
@@ -23,7 +35,10 @@
             </ul>
           </nav>
         </div>
+        <!-- End footer column 2 (main nav links) -->
 
+
+        <!-- Start footer column 3 (secondary nav links) -->
         <div class="site-footer__col-three">
           <h3 class="headline headline--small">Learn</h3>
           <nav class="nav-list">
@@ -33,8 +48,13 @@
             </ul>
           </nav>
         </div>
-      </div>
+        <!-- End footer column 3 (secondary nav links) -->
 
+      </div>
+      <!-- End footer nav links -->
+
+
+      <!-- Start footer column 4 (social media icons) -->
       <div class="site-footer__col-four">
         <h3 class="headline headline--small">Connect With Us</h3>
         <nav>
@@ -48,8 +68,12 @@
         </nav>
       </div>
     </div>
+    <!-- End footer column 4 (social media icons) -->
 
   </div>
 </footer>
+
+<!-- End footer div -- >
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <?php wp_head();?>
 
+    <?php wp_head();?>
     <meta charset="utf-8">
     <title></title>
+
   </head>
+  
   <body>
+
+    <!-- Start header div -->
     <header class="site-header">
     <div class="container">
       <h1 class="logo-text float-left"><a href="<?php echo site_url();?>"><strong>Miracle</strong> Mob</a></h1>
       <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
       <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
       <div class="site-header__menu group">
+
+
+        <!-- Start nav links -->
         <nav class="main-navigation">
           <ul>
             <li><a href="<?php echo site_url('/about-us');?>">About Us</a></li>
@@ -22,11 +29,17 @@
             <li><a href="#">Contact</a></li>
           </ul>
         </nav>
+        <!-- End nav links -->
+
+        <!-- Start login / register buttons -->
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
           <a href="#" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
           <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         </div>
+        <!-- End login / register buttons -->
+
       </div>
     </div>
-  </header>
+    </header>
+    <!-- End header div -->
